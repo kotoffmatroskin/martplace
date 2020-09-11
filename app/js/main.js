63f639bcf7a153c5ac5a4a513607de6749f3b7da
kotoffmatroskin <kotoffmatroskin@gmail.com>
2020-09-11 16:26:12 +0300

    add section weekly-products

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,11 @@
 $(function () {
 
+  $('.weekly-products__inner').slick({
+    arrows: true,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/products/prev.png" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/products/next.png" alt=""></button>'
 
+  });
 
 
 })
