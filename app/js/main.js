fe96b1708f94250d9537a973d75eacede1d0f8a0
kotoffmatroskin <kotoffmatroskin@gmail.com>
2020-09-29 15:56:32 +0300

    fix index.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,26 @@
 $(function () {
 
-  $('.weekly-products__inner').slick({
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    starWidth: "15px",
+    spacing: "3px",
+    ratedFill: "#ffc000",
+    normalFill: "#ffffff",
+    readOnly: true
+  });
+
+  $('.weekly-product__inner').slick({
     arrows: true,
     prevArrow: '<button class="slick-arrow slick-prev"><img src="images/products/prev.png" alt=""></button>',
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/products/next.png" alt=""></button>'
 
   });
 
-  var mixer = mixitup('.products__inner-box');
+  $('.digital-product__select').on('click', function () {
+    $('.digital-product__select').toggleClass('active');
+  });
+
+
+  var mixer = mixitup('.release-products__inner-box');
 
 })
